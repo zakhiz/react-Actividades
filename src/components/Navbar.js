c4b9986d7384b55e222cc389e21f6ab2d53e5aed
jimmy sebastian higa ramirez <sebascursoshtml@gmail.com>
2022-09-02 20:28:35 -0300

    actividad numero 1,2,3 completadas

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+const Navbar=()=>{
+    return( 
+    <>
+    <nav className="btn-primary">
+        <a className="titulo" href="#">VeoVeo</a>
+      <ul>
+      <li>
+        <a  href="#">Marcas</a>
+      </li>
+      <li>
+        <a  href="#">Lentes de sol</a>
+      </li>
+      <li>
+        <a  href="#">Protesis oculares</a>
+      </li>
+      </ul>
+    </nav>
+    </>
+    );
+}
+export default Navbar;
